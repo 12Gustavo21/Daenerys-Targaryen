af87eb24a5dafaf8aa2a88fa739328638e4e9342
Sky <skyvieira.dev@gmail.com>
2025-04-13 03:56:06 -0300

    feat: implement locomotive-scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,50 @@
-import { lazy, Suspense } from "react";
+import { lazy, Suspense, useEffect } from "react";
+import LocomotiveScroll from "locomotive-scroll";
 
 // global Style
 import { GlobalStyle } from "./assets/global/style";
 
 // components
 import Header from "./assets/components/header";
-const Stormborn = lazy(() => import("./assets/components/titles/stormborn"))
-const Targaryen = lazy(() => import("./assets/components/titles/targaryen"))
-const Queen = lazy(() => import("./assets/components/titles/queen"))
-const Protector = lazy(() => import("./assets/components/titles/protector"))
-const Mother = lazy(() => import("./assets/components/titles/mother"))
+const Stormborn = lazy(() => import("./assets/components/titles/stormborn"));
+const Targaryen = lazy(() => import("./assets/components/titles/targaryen"));
+const Queen = lazy(() => import("./assets/components/titles/queen"));
+const Protector = lazy(() => import("./assets/components/titles/protector"));
+const Mother = lazy(() => import("./assets/components/titles/mother"));
 
 export default function App() {
+  useEffect(() => {
+    const scroll = new LocomotiveScroll({
+      el: document.querySelector(".scroll-container"),
+      smooth: true,
+      lerp: 0.01,
+      scrollFromAnywhere: true,
+    });
+
+    return () => scroll.destroy();
+  }, []);
+
   return (
     <>
       <GlobalStyle />
-      <Header />
-      <Suspense fallback={<div>loading ...</div>}>
-        <Stormborn />
-      </Suspense>
-      <Suspense fallback={<div>loading ...</div>}>
-        <Targaryen />
-      </Suspense>
-      <Suspense fallback={<div>loading ...</div>}>
-        <Queen />
-      </Suspense>
-      <Suspense fallback={<div>loading ...</div>}>
-        <Protector />
-      </Suspense>
-      <Suspense fallback={<div>loading ...</div>}>
-        <Mother />
-      </Suspense>
+      <div className="scroll-container">
+        <Header />
+        <Suspense fallback={<div>loading ...</div>}>
+          <Stormborn />
+        </Suspense>
+        <Suspense fallback={<div>loading ...</div>}>
+          <Targaryen />
+        </Suspense>
+        <Suspense fallback={<div>loading ...</div>}>
+          <Queen />
+        </Suspense>
+        <Suspense fallback={<div>loading ...</div>}>
+          <Protector />
+        </Suspense>
+        <Suspense fallback={<div>loading ...</div>}>
+          <Mother />
+        </Suspense>
+      </div>
     </>
-  )
+  );
 }
